d594e54606af1d4a859f235538e92161f88f2eab
zerik <zerikx@gmail.com>
2024-01-09 11:25:55 -0800

    full func

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
+let apiQuotes = [];
+
+// show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+// New quote
+
+function newQuote() {
+  loading();
+  // Pick Random Quote from apiQuotes
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if aouthor is blank or unkknown
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.lenght > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  loading();
+  const apiURL = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch error
+  }
+}
+
+//Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuotes();
